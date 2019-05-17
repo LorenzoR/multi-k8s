4795ec8685b0884b8b267d33298c3d573ce54545
Lorenzo Rodrigo <lrodrigo@gmail.com>
2019-05-17 23:14:25 +1000

    2 ingress

--- server/index.js
+++ server/index.js
@@ -36,13 +36,13 @@ const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
-app.get('/values/all', async (req, res) => {
+app.get('/values/all/', async (req, res) => {
   const values = await pgClient.query('SELECT * FROM values');
 
   res.send(values.rows);
 });
 
-app.get('/values/current', async (req, res) => {
+app.get('/values/current/', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
@@ -57,7 +57,7 @@ app.get('/test', (req, res) => {
   res.send('Hi test');
 });
 
-app.post('/values', async (req, res) => {
+app.post('/values/', async (req, res) => {
   const index = req.body.index;
 
   if ( parseInt(index) > 40 ) {
